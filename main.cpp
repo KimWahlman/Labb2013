660e5f6274d9c9fe17ce0445b32667dbc9ececbf
Johannes Sinander <johannes.sinander@gmail.com>
2013-10-13 17:37:20 +0200

    KLAR

--- main.cpp
+++ main.cpp
@@ -13,64 +13,99 @@ std::vector<__int64> setVector( std::vector<int>, std::vector<int>, int );
 int main()
 {
 	std::vector<int> coefficients, initialValues;
-	int n = 0, value = 0;
+	int n, k, d, value;
 
-	//Read n
-	std::cout << "Enter number of values (n): ";
-	while(n == 0)
+	while ( true )
 	{
-		std::cin >> n;
-		if( std::cin.fail() )
-		{
-			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
-			std::cin.clear();
-			std::cin.ignore();
-			n = 0;
+		n = 0;
+		k = 0;
+		value = 0;
 
+		//Read n
+		std::cout << "Enter the number of values (n): ";
+		while(n == 0)
+		{
+			std::cin >> n;
+			if( std::cin.fail() )
+			{
+				std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
+				std::cin.clear();
+				std::cin.ignore();
+				n = 0;
+			}
 		}
+
+		//Read d
+		/*std::cout << "Enter the number of coefficients (d): ";
+		while(d == 0)
+		{
+			std::cin >> d;
+			if( std::cin.fail() || n > d - 1 )
+			{
+				std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
+				std::cin.clear();
+				std::cin.ignore();
+				d = 0;
+			}
+		}*/
+
+		//Read coefficients
+		std::cout << "Enter " << n - 1 << " cofficients: ";
+		for( int i = 0; i < n - 1; ++i )
+		{
+			std::cin >> value;
+			if( std::cin.fail() )
+			{
+				std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
+				std::cin.clear();
+				std::cin.ignore();
+				i--;
+			}
+			else
+			{
+				coefficients.push_back( value );
+			}
+		}
+	
+		//Read k
+		std::cout << "Enter the number of initial values (k): ";
+		while(k == 0)
+		{
+			std::cin >> k;
+			if( std::cin.fail() )
+			{
+				std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
+				std::cin.clear();
+				std::cin.ignore();
+				k = 0;
+
+			}
+		}
+
+		//Read 2 initial values
+		std::cout << "Enter " << k << " initial values: ";
+		for( int i = 0; i < k; ++i )
+		{
+			std::cin >> value;
+			if( std::cin.fail() )
+			{
+				std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
+				std::cin.clear();
+				std::cin.ignore();
+				i--;
+			}
+			else
+			{
+				initialValues.push_back( value );
+			}
+		}
+
+		setVector( coefficients, initialValues, n );
+
+		std::cout << "\n\n\n";
+		/*std::cin.get();
+		std::cin.get();*/
 	}
-
-	//Read coefficients
-	std::cout << "Enter " << n << " cofficients: ";
-	for( int i = 0; i < n; ++i )
-	{
-		std::cin >> value;
-		if( std::cin.fail() )
-		{
-			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
-			std::cin.clear();
-			std::cin.ignore();
-			i--;
-		}
-		else
-		{
-			coefficients.push_back( value );
-		}
-	}
-
-	//Read 2 initial values
-	std::cout << "Enter " << 2 << " initial values: ";
-	for( int i = 0; i < 2; ++i )
-	{
-		std::cin >> value;
-		if( std::cin.fail() )
-		{
-			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
-			std::cin.clear();
-			std::cin.ignore();
-			i--;
-		}
-		else
-		{
-			initialValues.push_back( value );
-		}
-	}
-
-	setVector( coefficients, initialValues, n );
-
-	std::cout << "Press enter to exit..";
-	std::cin.get();
-	std::cin.get();
 
 	return 0;
 }
@@ -90,10 +125,19 @@ std::vector<__int64> setVector( std::vector<int> coefficients, std::vector<int> 
 	for (int i = returnValues.size(); i < n; i++)
 	{
 		an = 0;
-		for (int j = 0; j < 2; j++)
+		/*for (int j = 0; j < 2; j++)
 		{
-			an += coefficients[i - returnValues.size() + j] * returnValues[ returnValues.size() - (j + 1) ];
+			
+			an += coefficients[i + j - initialValues.size()] * returnValues[ returnValues.size() - (j + 1) ];
+			std::cout << coefficients[i + j - initialValues.size()] << " * " << returnValues[ returnValues.size() - (j + 1) ] << " = " << coefficients[i - returnValues.size() + j] * returnValues[ returnValues.size() - (j + 1) ] << " Total: " << an << "\n";
+		}*/
+		for (int j = 0; j < returnValues.size(); j++)
+		{
+			
+			an += coefficients[j] * returnValues[ returnValues.size() - (j + 1) ];
+			std::cout << "\n" << coefficients[j] << " * " << returnValues[ returnValues.size() - (j + 1) ] << " = " << coefficients[i - returnValues.size() + j] * returnValues[ returnValues.size() - (j + 1) ] << " Total: " << an;
 		}
+		std::cout << "\n\n";
 		returnValues.push_back( an );
 	}
 	
