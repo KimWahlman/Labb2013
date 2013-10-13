e7c8a26a009ddd699fb53809acb28b5d78d0006a
Johannes Sinander <johannes.sinander@gmail.com>
2013-10-13 15:13:45 +0200

    Ny main.cpp

--- main.cpp
+++ main.cpp
@@ -1,93 +1,108 @@
+//////////////////////////////////////////////////////////
+//	Authors:											//
+//	Kim Wahlman			kim.wahlman@gmail.com			//
+//	Johannes Sinander	johannes.sinander@gmail.com		//
+//	Jacob Fogelberg										//
+//////////////////////////////////////////////////////////
 #include <iostream>
 #include <vector>
 #include <cmath>
 
-/**********************************************
-	Developers
-	Kim Wahlman			kim.wahlman@gmail.com
-	Johannes Sinander	johannes.sinander@gmail.com
-	Jacob Fogelberg
-***********************************************/
-
-// ce = coefficents, iv = initial value, n = # of numbers
-std::vector<long long unsigned int> setVector(std::vector<long long unsigned int> ce, std::vector<long long unsigned int> iv, int n)
-{
-	std::vector<long long unsigned int> returnValues;
-	returnValues = iv;
-
-	long long unsigned int an = 0;
-	for (int i = returnValues.size(); i < n; i++)
-	{
-		an = 0;
-		//std::cout << "returnValue.size = " << returnValues.size() << std::endl;
-		for(int j = 0; j < 2; j++)
-		{
-			an += ce[i - returnValues.size() + j] * returnValues[ returnValues.size() - (j + 1) ];
-		}
-		returnValues.push_back(an);
-		// a3 = c1a(n-1)
-		//an = ce[i] * iv[i];
-	}
-	std::cout << std::endl;
-	for(std::vector<long long unsigned int>::iterator it = returnValues.begin(); it != returnValues.end(); it++)
-	{
-		std::cout << (*it) << std::endl;
-	}
-	std::cin >> an;
-	return returnValues;
-}
+std::vector<__int64> setVector( std::vector<int>, std::vector<int>, int );
 
 int main()
 {
-	std::vector<long long unsigned int> ce, iv;
+	std::vector<int> coefficients, initialValues;
 	int n = 0, value = 0;
 
-	std::cout << "Enter amount of values to do stuff with: ";
+	//Read n
+	std::cout << "Enter number of values (n): ";
 	while(n == 0)
 	{
 		std::cin >> n;
-		if(std::cin.fail())
+		if( std::cin.fail() )
 		{
 			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
 			std::cin.clear();
 			std::cin.ignore();
 			n = 0;
+
 		}
-	}
-	
-	std::cout << "Enter " << n << " cofficient: ";
-	for(int i = 0; i < n; ++i)
-	{
-		
-		std::cin >> value;
-		if(std::cin.fail())
-		{
-			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
-			std::cin.clear();
-			std::cin.ignore();
-			i--;
-		}
-		else
-			ce.push_back(value);
-	}
-	
-	std::cout << "Enter two initial values: ";
-	for(int i = 0; i < 2; ++i)
-	{
-		
-		std::cin >> value;
-		if(std::cin.fail())
-		{
-			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
-			std::cin.clear();
-			std::cin.ignore();
-			i--;
-		}
-		else
-			iv.push_back(value);
 	}
 
-	setVector(ce, iv, 100);
+	//Read coefficients
+	std::cout << "Enter " << n << " cofficients: ";
+	for( int i = 0; i < n; ++i )
+	{
+		std::cin >> value;
+		if( std::cin.fail() )
+		{
+			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
+			std::cin.clear();
+			std::cin.ignore();
+			i--;
+		}
+		else
+		{
+			coefficients.push_back( value );
+		}
+	}
+
+	//Read 2 initial values
+	std::cout << "Enter " << 2 << " initial values: ";
+	for( int i = 0; i < 2; ++i )
+	{
+		std::cin >> value;
+		if( std::cin.fail() )
+		{
+			std::cout << "STOP WRITING NON-NUMBERS BAKA!\n";
+			std::cin.clear();
+			std::cin.ignore();
+			i--;
+		}
+		else
+		{
+			initialValues.push_back( value );
+		}
+	}
+
+	setVector( coefficients, initialValues, n );
+
+	std::cout << "Press enter to exit..";
+	std::cin.get();
+	std::cin.get();
 
 	return 0;
+}
+
+//////////////////
+//	FUNCTIONS	//
+//////////////////
+std::vector<__int64> setVector( std::vector<int> coefficients, std::vector<int> initialValues, int n )
+{
+	std::vector<__int64> returnValues;
+	for (std::vector<int>::iterator it = initialValues.begin(); it != initialValues.end(); it++)
+	{
+		returnValues.push_back( (*it) );
+	}
+	
+	__int64 an;
+	for (int i = returnValues.size(); i < n; i++)
+	{
+		an = 0;
+		for (int j = 0; j < 2; j++)
+		{
+			an += coefficients[i - returnValues.size() + j] * returnValues[ returnValues.size() - (j + 1) ];
+		}
+		returnValues.push_back( an );
+	}
+	
+	std::cout << "Result: ";
+	for ( std::vector<__int64>::iterator it = returnValues.begin(); it != returnValues.end(); it++ )
+	{
+		std::cout << (*it) << " ";
+	}
+	std::cout << "\n";
+
+	return returnValues;
 }
